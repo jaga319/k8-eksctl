735f77cd46174fe138c483852d00f797651c5f0d
jaga319 <mummadijagadeesh219@gmail.com>
2024-03-14 20:22:34 +0530

    workstation

--- workstation.tf
+++ workstation.tf
@@ -1,21 +1,19 @@
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "work-station-eksctl"
-
+  name = "workstation-eksctl"
+  ami = data.aws_ami.centos8.id
   instance_type          = "t2.micro"
-#   key_name               = "user1"
-#   monitoring             = true
-  ami= data.aws_ami.centos.id
+  #key_name               = "user1"
+  #monitoring             = true
   vpc_security_group_ids = [aws_security_group.allow_eksctl.id]
-  subnet_id              = "subnet-01ceef28efa3e7266"
+  subnet_id              = "subnet-01ceef28efa3e7266" #replace your default subnet id
   user_data = file("workstation.sh")
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
 }
-
 
 resource "aws_security_group" "allow_eksctl" {
   name        = "allow_eksctl"
@@ -40,20 +38,22 @@ resource "aws_security_group" "allow_eksctl" {
   }
 }
 
-data "aws_ami" "centos"{
-  owners = ["973714476881"]
-  most_recent = true
-  filter {
-    name ="name"
-    values = ["Centos-8-DevOps-Practice"]
+data "aws_ami" "centos8"{
+    owners = ["973714476881"]
+    most_recent      = true
 
-  }
-  filter {
-    name="root-device-type"
-    values = ["ebs"]
-  }
-  filter {
-    name = "virtualization-type"
-    values = ["hvm"]
-  }
+    filter {
+        name   = "name"
+        values = ["Centos-8-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
 }
